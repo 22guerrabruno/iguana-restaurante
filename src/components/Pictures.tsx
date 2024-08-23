dda7caa9e83d909bcb581927089269d41753f9c9
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-08-23 18:34:18 +0200

    upload images

--- src/components/Pictures.tsx
+++ src/components/Pictures.tsx
@@ -4,63 +4,63 @@ const Pictures = () => {
     <div className='w-full grid grid-cols-2 md:grid-cols-3 gap-2'>
       <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
         <img
-          src='/pulpo2.jpg'
-          alt=''
-          className='w-full h-full object-cover zoom-in-150'
-        />
-      </div>
-      <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
-        <img
-          src='/mollejas2.jpg'
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417645/pulpo2_e2pdww.jpg'
           alt=''
           className='w-full h-full object-cover'
         />
       </div>
       <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
         <img
-          src='/calamares.jpg'
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417644/mollejas2_vzeznr.jpg'
           alt=''
           className='w-full h-full object-cover'
         />
       </div>
       <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
         <img
-          src='/Mejillones.jpg'
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417638/calamares_huastd.jpg'
           alt=''
           className='w-full h-full object-cover'
         />
       </div>
       <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
         <img
-          src='/tataki.jpg'
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417641/Mejillones_r0lnnd.jpg'
           alt=''
           className='w-full h-full object-cover'
         />
       </div>
       <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
         <img
-          src='/costillas.jpg'
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417646/tataki_pq5qg5.jpg'
           alt=''
           className='w-full h-full object-cover'
         />
       </div>
       <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
         <img
-          src='/gambones.jpg'
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417639/costillas_cimde4.jpg'
           alt=''
           className='w-full h-full object-cover'
         />
       </div>
       <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
         <img
-          src='/sashimi.jpg'
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417640/gambones_dux81l.jpg'
           alt=''
           className='w-full h-full object-cover'
         />
       </div>
       <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
         <img
-          src='/panacotta.jpg'
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417645/sashimi_nifmrt.jpg'
+          alt=''
+          className='w-full h-full object-cover'
+        />
+      </div>
+      <div className='flex items-center justify-center h-[250px] border-4 border-black border-collapse rounded-lg'>
+        <img
+          src='https://res.cloudinary.com/duihhp37l/image/upload/v1724417645/panacotta_t5h4sw.jpg'
           alt=''
           className='w-full h-full object-cover'
         />
