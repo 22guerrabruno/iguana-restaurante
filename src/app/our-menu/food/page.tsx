fee5ed79fffee11d845075353906e5130a881ea5
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-08-28 11:21:14 +0200

    change to food page

--- src/app/our-menu/food/page.tsx
+++ src/app/our-menu/food/page.tsx
@@ -1,10 +1,11 @@
 const FoodMenuPage = () => {
   return (
-    <div className='w-full flex flex-col items-center justify-center gap-2 p-4'>
+    <div className='w-full flex flex-col items-center justify-center gap-2 p-4 h-full'>
       <iframe
         src='/comida-iguana.pdf'
         width={300}
         height={900}
+        allowFullScreen
         className='w-full'></iframe>
     </div>
   );
